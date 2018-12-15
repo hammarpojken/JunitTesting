d16fdc166e8ea3935a00e6d99f1b4a0ce7c85dc3
hammarpojken <nicke_hammar@hotmail.com>
2018-12-15 10:31:00 +0100

    Add tests for StringHelper.TurncateAinFirst2Positions()

--- src/test/java/testCode/StringHelperTest.java
+++ src/test/java/testCode/StringHelperTest.java
@@ -5,10 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class StringHelperTest {
+	
+	StringHelper helper = new StringHelper();
 
 	@Test
-	public void test() {
-		
+	public void testTruncateAInFirst2Positions_Condition1() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("AACD"));
+	}
+	
+	@Test
+	public void testTruncateAInFirst2Positions_Condition2() {
+		assertEquals("CD", helper.truncateAInFirst2Positions("ACD"));
+	}
+	
+	@Test
+	public void testTurncateAinFIrst2Postitions_Condition3() {
+		assertEquals("CDEF", helper.truncateAInFirst2Positions("CDEF"));
+	}
+	@Test
+	public void testTurncateAinFIrst2Postitions_Condition4() {
+		assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
 	}
 
 }
