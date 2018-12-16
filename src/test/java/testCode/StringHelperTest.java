9ebf285bb0501ac7f9fb4401ab73fba4cfbd451e
hammarpojken <nicke_hammar@hotmail.com>
2018-12-16 16:09:05 +0100

    Add tests for areFirstAnd2LastCharactersTheSame()

--- src/test/java/testCode/StringHelperTest.java
+++ src/test/java/testCode/StringHelperTest.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 public class StringHelperTest {
 	
 	StringHelper helper = new StringHelper();
+	
+	// Tests for TruncateAInFirst2Positions()
 
 	@Test
 	public void testTruncateAInFirst2Positions_Condition1() {
@@ -25,6 +27,29 @@ public class StringHelperTest {
 	@Test
 	public void testTurncateAinFIrst2Postitions_Condition4() {
 		assertEquals("CDAA", helper.truncateAInFirst2Positions("CDAA"));
+	}
+	
+	// Tests for areFirstAndLast2CharactersTheSame
+	
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_Condition1() {
+		assertFalse(helper.areFirstAndLastTwoCharactersTheSame("ABCD"));
+		
+	}
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_Condition2() {
+		assertTrue(helper.areFirstAndLastTwoCharactersTheSame("ABAB"));
+		
+	}
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_Condition3() {
+		assertTrue(helper.areFirstAndLastTwoCharactersTheSame("AB"));
+		
+	}
+	@Test
+	public void testAreFirstAndLastTwoCharactersTheSame_Condition4() {
+		assertFalse(helper.areFirstAndLastTwoCharactersTheSame("A"));
+		
 	}
 
 }
