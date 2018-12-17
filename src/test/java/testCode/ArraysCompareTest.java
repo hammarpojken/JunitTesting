0bb0036f0013b55b18fb22084a30ddb4971380d0
hammarpojken <nicke_hammar@hotmail.com>
2018-12-17 20:28:27 +0100

    Test for comparing arrays

---
+++ src/test/java/testCode/ArraysCompareTest.java
@@ -0,0 +1,21 @@
+package testCode;
+
+import static org.junit.Assert.*;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class ArraysCompareTest {
+
+	@Test
+	public void testArraySort_RandomArray() {
+		int[] numbers = { 2, 5, 6, 7, 4, 2, 6, 5 };
+		int[] expected = { 2, 2, 4, 5, 5, 6, 6, 7 };
+
+		Arrays.sort(numbers);
+	
+		assertArrayEquals(expected, numbers);
+	}
+
+}
--- src/test/java/testCode/BeforeAfterTest.java
+++ src/test/java/testCode/BeforeAfterTest.java
@@ -3,6 +3,7 @@ package testCode;
 
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
